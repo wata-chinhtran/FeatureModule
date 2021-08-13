2c148d0a80c5f6a92d7e34eda279d535c6d5020a
Jonny Chinh Tran <chinh.tran@watasolutions.com>
2021-08-13 11:42:55 +0700

    + add pod

---
+++ Pods/Target Support Files/CommonModule/CommonModule-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CommonModule.h"
+
+FOUNDATION_EXPORT double CommonModuleVersionNumber;
+FOUNDATION_EXPORT const unsigned char CommonModuleVersionString[];
+
